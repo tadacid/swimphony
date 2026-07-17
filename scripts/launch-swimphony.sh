2052fc96249b77e1e204ef493d0fc294684404be
tadacid <hello@tadahiro.design>
2026-07-18 03:17:55 +0900

    fix: reuse existing Swimphony tab

--- scripts/launch-swimphony.sh
+++ scripts/launch-swimphony.sh
@@ -41,7 +41,23 @@ if ! is_swimphony_ready; then
 fi
 
 if [[ -d "/Applications/Google Chrome.app" ]]; then
-  /usr/bin/open -a "Google Chrome" "$APP_URL"
+  /usr/bin/osascript <<'APPLESCRIPT'
+tell application "Google Chrome"
+  activate
+  repeat with browserWindow in windows
+    set tabNumber to 0
+    repeat with browserTab in tabs of browserWindow
+      set tabNumber to tabNumber + 1
+      if URL of browserTab starts with "http://localhost:3000/" then
+        set active tab index of browserWindow to tabNumber
+        set index of browserWindow to 1
+        return
+      end if
+    end repeat
+  end repeat
+  open location "http://localhost:3000/"
+end tell
+APPLESCRIPT
 else
   /usr/bin/open "$APP_URL"
 fi
